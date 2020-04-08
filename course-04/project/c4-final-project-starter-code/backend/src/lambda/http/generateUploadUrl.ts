60e92c013ece783b62a3e1d3c9cdb9b1c4cd2409
Antonette Caldwell <pullmana8@gmail.com>
2020-04-08 09:22:20 -0500

    Added signed url and xray

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/generateUploadUrl.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/generateUploadUrl.ts
@@ -1,10 +1,30 @@
-import 'source-map-support/register'
-
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import 'source-map-support/register'
+import * as AWS from 'aws-sdk'
+import * as AWSXray from 'aws-xray-sdk'
+
+const XAWS = AWSXRay.captureAWS(AWS)
+
+const bucketName = process.env.IMAGES_S3_BUCEKT
+const urlExpiration = process.env.SIGNED_URL_EXPIRATION
+
+const s3 = new XAWS.S3({
+  signatureVersion: 'v4'
+})
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
-
   // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+  const uploadUrl = getUploadUrl(todoId)
+  
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      uploadUrl
+    })
+  }
 }
