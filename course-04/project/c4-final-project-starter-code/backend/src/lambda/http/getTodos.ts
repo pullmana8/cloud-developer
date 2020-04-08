12484664d3d85846779d5bdac439d8401f26874e
Antonette Caldwell <pullmana8@gmail.com>
2020-04-08 08:38:53 -0500

    Added AWS X-Ray in comments

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
@@ -1,8 +1,12 @@
-import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import 'source-map-support/register'
 import * as AWS from 'aws-sdk'
+// import * as AWSXray from 'aws-xray-sdk'
+
+// const XAWS = AWSXRay.captureAWS(AWS)
 
 const docClient = new AWS.DynamoDB.DocumentClient()
+// const docClient = new XAWS.DynamoDB.DocumentClient()
 const todostTable = process.env.TODOS_TABLE
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
